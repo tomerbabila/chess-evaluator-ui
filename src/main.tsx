b4c4cb4a34b679379b77d897c7768d5a40459a9c
tomerbabila <tomer.babila@gmail.com>
2025-06-15 20:41:06 +0300

    Create new app

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './index.css';
+import App from './App.tsx';
+
+const queryClient = new QueryClient(); // You can pass options here, e.g., default query options
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
